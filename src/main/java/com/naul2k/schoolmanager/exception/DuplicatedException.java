e3a714d8ee76fa7a74ff68d5ddaabe6586320cae
ngckinhluan <nkluantran2907@gmail.com>
2024-09-02 13:03:14 +0700

    Fix CI/CD

--- src/main/java/com/naul2k/schoolmanager/exception/DuplicatedException.java
+++ src/main/java/com/naul2k/schoolmanager/exception/DuplicatedException.java
@@ -1,4 +1,4 @@
-﻿package com.naul2k.schoolmanager.exception;
+package com.naul2k.schoolmanager.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
